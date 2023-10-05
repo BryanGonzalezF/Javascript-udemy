83431a2bcff16d41cb003b7f9b7d106ccf97b279
Bryan Gonzalez <brygon.2015@gmail.com>
2023-10-05 09:39:13 -0600

    primeras clases y hola mundo

--- ClasesJs/Clase1/script.js
+++ ClasesJs/Clase1/script.js
@@ -3,4 +3,6 @@ console.log('Hola Mundo');
 alert('Hola');
 alert('Bienvenidos a este maravilloso mundo');
 
-document.write('Estoy muy feliz de iniciar');
+document.write(
+  'Estoy muy feliz de iniciar'
+); /* nos funciona para escribir dentro de nuestro html desde js  */
