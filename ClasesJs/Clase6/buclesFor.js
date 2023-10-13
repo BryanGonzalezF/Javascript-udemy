591e0a67ef8644c65856be05c3ccec36a6b92d72
Bryan Gonzalez <brygon.2015@gmail.com>
2023-10-13 09:38:05 -0600

    Clase 6  bucles for, while y do while

---
+++ ClasesJs/Clase6/buclesFor.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//Bucle for
+// Bucle es una estructura de control que se repite varias veces
+
+var numero = 100;
+
+for (let i = 0; i <= numero; i++) {
+  console.log('iteracion numero: ' + i);
+}
