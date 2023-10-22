288c9460c6c74ba5cf4e2f9733fb2161afe133e1
Bryan Gonzalez <brygon.2015@gmail.com>
2023-10-21 21:39:44 -0600

    cambios

---
+++ ClasesJs/Clase4/script4.js
@@ -0,0 +1,13 @@
+/* Tipos de datos */
+'use sctrict';
+
+var numero1 = 7;
+var numero2 = 3;
+var numerosSumados = numero1 + numero2;
+/* en una variable como la del ejemplo
+se puede utilizar la suma, resta 
+multiplicacion (*), division (/)
+y el residuo (%) como tambien 
+usar exponentes con (**) */
+
+alert('El resultado es' + ' ' + numerosSumados);
