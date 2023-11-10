0110c5748d9b6626cc8bdac3616970ed223e9a36
Bryan Gonzalez <brygon.2015@gmail.com>
2023-11-09 23:17:55 -0600

    Avance en el ejercicio del DOM

--- Ejercicios/EjercicioDOM/script.js
+++ Ejercicios/EjercicioDOM/script.js
@@ -2,12 +2,23 @@
 
 window.addEventListener('load', () => {
   let formulario = document.querySelector('#formInfo');
+  let boxDashed = document.querySelector('.dashed');
+
+  boxDashed.style.display = 'none';
 
   formulario.addEventListener('submit', () => {
     console.log('Evento submit capturado');
     let formNombre = document.querySelector('#nombre').value;
     let formApellido = document.querySelector('#apellido').value;
     let formEdad = document.querySelector('#edad').value;
-    console.log(formNombre, formApellido, formEdad);
+
+    let datoUsuario = [formNombre, formApellido, formEdad];
+    boxDashed.style.display = 'block';
+
+    for (let indice in datoUsuario) {
+      let parrafo = document.createElement('p');
+      parrafo.append(datoUsuario[indice]);
+      document.querySelector('.info').append(parrafo);
+    }
   });
 }); // fin del script
